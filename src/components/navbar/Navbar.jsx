37a5b663bc8097f2ee422dff05347743a7ec5919
Xhokabash <xhokabash@gmail.com>
2023-10-15 12:02:44 +0200

    fix a  minor bugs of styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -22,7 +22,7 @@ function Navbar() {
           <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
         </svg>
         <div className="main-logo">
-          <img src="./src/assets/bus-logo.png" alt="" />
+          <img src="./bus-logo.png" alt="" />
         </div>
 
         <ul>
