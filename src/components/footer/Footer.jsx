0b86fb96f5e7d382f2094cb56bd67911d2950537
Xhokabash <xhokabash@gmail.com>
2023-10-15 12:56:21 +0200

    add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,28 @@
+import "./footer.css";
+
 function Footer() {
   return (
     <>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-        aspernatur dignissimos reprehenderit animi, doloremque culpa ea
-        voluptatem praesentium a perspiciatis. Quisquam molestias earum animi
-        eum quibusdam corrupti porro ipsam mollitia!
+      <div className="footer-container">
+        <div className="footer-logos">
+          <img src="./social-logo/facebook.png" alt="" />
+          <img src="./social-logo/instagram.png" alt="" />
+          <img src="./social-logo/twitter.png" alt="" />
+        </div>
+        <div className="footer-contact">
+          <div>
+            <p>Privacy policy </p>
+            <p>General conditions </p>
+          </div>
+          <div>
+            <p>Need help? </p>
+            <p>Contact Us</p>
+          </div>
+          <div>
+            <p>Who we are</p>
+            <p>Careers </p>
+          </div>
+        </div>
       </div>
     </>
   );
