5ede9e983017f2224583a6235c36d1fe5da7fa19
Xhokabash <xhokabash@gmail.com>
2023-11-11 01:21:20 +0100

    add about us page

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -1,13 +1,31 @@
 import Cookies from "universal-cookie";
 import { AuthContext } from "../../Context";
 import { useContext, useEffect } from "react";
+import styles from "./About.module.css";
+import { en, sq, it, de } from "./about.data";
+import Lang from "../../Context";
+import { v4 as uuidv4 } from "uuid";
 
 const cookies = new Cookies();
 
 function About() {
   const token = cookies.get("TOKEN");
   const { setUser } = useContext(AuthContext);
+  const { data } = useContext(Lang);
 
+  let textdata;
+
+  if (data === "eng") {
+    textdata = en;
+  } else if (data === "de") {
+    textdata = de;
+  } else if (data === "sq") {
+    textdata = sq;
+  } else if (data === "ita") {
+    textdata = it;
+  }
+
+  console.log(textdata);
   useEffect(() => {
     if (token) {
       // Set the user as authenticated
@@ -15,7 +33,22 @@ function About() {
     }
   }, [token, setUser]);
 
-  return <div>About</div>;
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.imageTop}></div>
+        <div>
+          {textdata.map((text) => (
+            <div className={styles.section} key={uuidv4()}>
+              <h3>{text.title}</h3>
+              <p className={styles.missionText}>{text.text}</p>
+            </div>
+          ))}
+        </div>
+        <div className={styles.image}></div>
+      </div>
+    </>
+  );
 }
 
 export default About;
---
+++ client/src/pages/about/about.data.js
@@ -0,0 +1,40 @@
+export const en = [
+  {
+    title: "About Us",
+    text: "Welcome to our bus ticket agency! At ASET, our mission is to provide convenient and reliable bus transportation services to our customers. We understand the importance of seamless travel, and we are committed to making your journey comfortable and enjoyable.",
+  },
+  {
+    title: "Our Mission",
+    text: "Our mission is to connect people and places through safe and efficient bus travel. We strive to be the preferred choice for individuals and families looking for hassle-free transportation solutions. Whether its a daily commute or a long-distance trip, we aim to make your travel experience smooth and memorable.",
+  },
+];
+export const sq = [
+  {
+    title: "Rreth Nesh",
+    text: "Mirësevini në agjencinë tonë të biletave autobusi! Në ASET, misioni ynë është të sigurojmë shërbime transporti të rehatshme dhe të besueshme me autobus për klientët tanë. Ne kuptojmë rëndësinë e udhëtimit të pa penguar, dhe jemi të përkushtuar që udhëtimi juaj të jetë i këndshëm dhe i kënaqshëm.",
+  },
+  {
+    title: "Misioni Ynë",
+    text: "Misioni ynë është të lidhim njerëzit dhe vendet përmes udhëtimit të sigurt dhe efikas me autobus. Ne përpiqemi të jemi zgjedhja e preferuar për individë dhe familje që kërkojnë zgjidhje të pa lodhshme për transport. Pavarësisht nëse është një udhëtim i përditshëm apo një udhëtim larg, synojmë që eksperienca juaj e udhëtimit të jetë e qetë dhe e paharrueshme.",
+  },
+];
+export const it = [
+  {
+    title: "Chi Siamo",
+    text: "Benvenuti nella nostra agenzia di biglietti per autobus! Da ASET, la nostra missione è fornire servizi di trasporto autobus comodi e affidabili ai nostri clienti. Comprendiamo l'importanza di un viaggio senza intoppi e ci impegniamo a rendere il vostro viaggio confortevole e piacevole.",
+  },
+  {
+    title: "La Nostra Missione",
+    text: "La nostra missione è connettere persone e luoghi attraverso viaggi in autobus sicuri ed efficienti. Cerchiamo di essere la scelta preferita per individui e famiglie che cercano soluzioni di trasporto senza problemi. Che si tratti di una corsa quotidiana o di un viaggio a lunga distanza, miriamo a rendere la vostra esperienza di viaggio fluida e memorabile.",
+  },
+];
+export const de = [
+  {
+    title: "Über Uns",
+    text: "Herzlich willkommen in unserer Bus-Ticket-Agentur! Bei ASET ist unsere Mission, bequeme und zuverlässige Bus-Transportdienstleistungen für unsere Kunden anzubieten. Wir verstehen die Bedeutung von reibungslosem Reisen und sind entschlossen, Ihre Reise angenehm und genießbar zu gestalten.",
+  },
+  {
+    title: "Unsere Mission",
+    text: "Unsere Mission ist es, Menschen und Orte durch sicheres und effizientes Busreisen zu verbinden. Wir streben danach, die bevorzugte Wahl für Einzelpersonen und Familien zu sein, die nach problemlosen Transportlösungen suchen. Ob es sich um den täglichen Weg zur Arbeit oder eine Langstreckenreise handelt, wir wollen Ihr Reiseerlebnis reibungslos und unvergesslich gestalten.",
+  },
+];
