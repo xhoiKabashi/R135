e3b0a03460280abe52a244ff4eb707d5cfcd1a22
Xhokabash <xhokabash@gmail.com>
2023-12-08 23:59:59 +0100

    make ready for deployed

--- client/src/pages/contact/Contact.jsx
+++ client/src/pages/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import styles from "./Contact.module.css";
+import styles from "./contact.module.css";
 import Img from "../../components/imgComponents/Img";
 import TextComponents from "../../components/textcomponents/TextComponents";
 import { useContext } from "react";
